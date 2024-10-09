2552cfab5f0cf49d2f40f36724cf0368a766f1c7
Pablo Teyssiere <teyssiere.lupab24@triana.salesianos.edu>
2024-10-09 12:21:33 +0200

    feat: cambios visuales y js PokeApi

--- js/PokeApi.js
+++ js/PokeApi.js
@@ -1,39 +1,45 @@
 $(document).ready(function () {
+  getPokemonListV2();
+
+  $(document).on("click", "#btn-get-data", function () {
     getPokemonListV2();
-  
-    $(document).on("click", "#btn-get-data", function () {
-      getPokemonListV2();
-    });
-   
-  
-    function getPokemonListV2() {
-      $(".pokemon-grid").html('<img src="../img/refresh giff.gif"></img>');
-      $.ajax({
-        url: "https://pokeapi.co/api/v2/pokemon",
-        method: "GET",
-      }).done(function (resp) {
-        setTimeout(function () {
-          $(".pokemon-grid").html("");
-          var listadoPomemon = resp.results;
-          listadoPomemon.forEach(function (pokemon) {
-            var pokemonId = pokemon.url.split("/")[6];
+  });
+
+
+  function getPokemonListV2() {
+    $(".pokemon-grid").html('<img src="../img/refresh giff.gif"></img>');
+    $.ajax({
+      url: "https://pokeapi.co/api/v2/pokemon?limit=151",
+      method: "GET",
+    }).done(function (resp) {
+      setTimeout(function () {
+        $(".pokemon-grid").html("");
+        var listadoPomemon = resp.results;
+        listadoPomemon.forEach(function (pokemon) {
+          var pokemonId = pokemon.url.split("/")[6];
+
+          $.ajax({
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`,
+            method: "GET",
+          }).done(function (pokemonDetails) {
+            var tipos = pokemonDetails.types.map(typeInfo => `<p class="pokemon-type ${typeInfo.type.name}">${typeInfo.type.name}</p>`).join("");
             var template = `
               <a href="pokejeta.html?pid=${pokemonId}"> 
               <div class="pokemon-card">
                 <div><p><Strong>${pokemon.name}</Strong></p></div>
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/${pokemonId}.png" alt=${pokemon.name}>
-                    <div class="tipos d-flex gap-2 align-items-center" >
-                        <p class="pokemon-type agua">${pokemon.tipos}</p>
+                  <div class="tipos d-flex gap-2 align-items-center" >
+                    ${tipos}
                 </div>
             </div>
             </a>
             `;
             $(".pokemon-grid").append(template);
           });
-        }, 1000);
-      });
-    }
-  });
+        });
+      }, 1000);
+    });
+  }
+});
 
 
- 
--- js/Pokejeta.js
+++ js/Pokejeta.js
@@ -2,9 +2,26 @@ $(document).ready(function () {
     var urlParams = new URLSearchParams(window.location.search);
     console.log(urlParams.get("pid")); 
     var pokemonId = urlParams.get("pid");
-  
+
     if (pokemonId == null) {
       alert("No se ha recibido el ID de pokemon");
+    } else {
+      getPokemonId(pokemonId);
     }
-  });
+  
+    function getPokemonId(pokemonId){
+      $.ajax({
+        url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`,
+        method: "GET",
+      }).done(function(resp){
+          var pokemon = resp; 
+          var template = `<h1>${pokemon.name}</h1>`; 
+          $(".titulo").append(template); 
+      }).fail(function(){
+          alert("Error al obtener los datos del Pokémon");
+      });
+    }
+
+
+});
 
